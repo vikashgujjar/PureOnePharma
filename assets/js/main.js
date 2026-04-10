f5dd89b0fa2137c81a607a33a2dab8a4aa09092d
Vikas Gujjar <www.vgujjar1234@gmail.com>
2026-04-10 21:04:48 +0530

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,387 @@
+(function ($) {
+  'use strict';
+
+  /*
+  |--------------------------------------------------------------------------
+  | Template Name: Pure One Pharma
+  | Author: ThemeServices
+  | Version: 1.0.0
+  |--------------------------------------------------------------------------
+  |--------------------------------------------------------------------------
+  | TABLE OF CONTENTS:
+  |--------------------------------------------------------------------------
+  |
+  | 1. Preloader
+  | 2. Mobile Menu
+  | 3. Sticky Header
+  | 4. Dynamic Background
+  | 5. Slick Slider
+  | 6. Modal Video
+  | 7. Scroll Up
+  | 8. Counter Animation
+  | 9. Tabs
+  | 10. Progress Bar
+  | 11. Review
+  | 
+  */
+
+  /*--------------------------------------------------------------
+    Scripts initialization
+  --------------------------------------------------------------*/
+  $.exists = function (selector) {
+    return $(selector).length > 0;
+  };
+
+  $(window).on('load', function () {
+    preloader();
+  });
+
+  $(function () {
+    mainNav();
+    stickyHeader();
+    dynamicBackground();
+    counterInit();
+    slickInit();
+    modalVideo();
+    scrollUp();
+    tabs();
+    progressBar();
+    review();
+
+    if ($.exists('.wow')) {
+      new WOW().init();
+    }
+  });
+
+  $(window).on('scroll', function () {
+    showScrollUp();
+  });
+
+  /*--------------------------------------------------------------
+    1. Preloader
+  --------------------------------------------------------------*/
+  function preloader() {
+    $('.cs_preloader').fadeOut();
+    $('cs_preloader_in').delay(150).fadeOut('slow');
+  }
+
+  /*--------------------------------------------------------------
+    2. Mobile Menu
+  --------------------------------------------------------------*/
+  function mainNav() {
+    $('.cs_nav').append('<span class="cs_menu_toggle"><span></span></span>');
+    $('.menu-item-has-children').append(
+      '<span class="cs_menu_dropdown_toggle"><span></span></span>',
+    );
+    $('.cs_menu_toggle').on('click', function () {
+      $(this)
+        .toggleClass('cs_toggle_active')
+        .siblings('.cs_nav_list')
+        .toggleClass('cs_active');
+    });
+    $('.cs_menu_toggle')
+      .parents('body')
+      .find('.cs_side_header')
+      .addClass('cs_has_main_nav');
+    $('.cs_menu_toggle')
+      .parents('body')
+      .find('.cs_toolbox')
+      .addClass('cs_has_main_nav');
+    $('.cs_menu_dropdown_toggle').on('click', function () {
+      $(this).toggleClass('active').siblings('ul').slideToggle();
+      $(this).parent().toggleClass('active');
+    });
+
+    /* Search Toggle */
+    $('.cs_search_toggle').on('click', function () {
+      $('.cs_header_search_form').toggleClass('active');
+    });
+  }
+
+  /*--------------------------------------------------------------
+    3. Sticky Header
+  --------------------------------------------------------------*/
+  function stickyHeader() {
+    var $window = $(window);
+    var lastScrollTop = 0;
+    var $header = $('.cs_sticky_header');
+    var headerHeight = $header.outerHeight() + 20;
+
+    $window.scroll(function () {
+      var windowTop = $window.scrollTop();
+
+      if (windowTop >= headerHeight) {
+        $header.addClass('cs_gescout_sticky');
+      } else {
+        $header.removeClass('cs_gescout_sticky');
+        $header.removeClass('cs_gescout_show');
+      }
+
+      if ($header.hasClass('cs_gescout_sticky')) {
+        if (windowTop < lastScrollTop) {
+          $header.addClass('cs_gescout_show');
+        } else {
+          $header.removeClass('cs_gescout_show');
+        }
+      }
+      lastScrollTop = windowTop;
+    });
+  }
+
+  /*--------------------------------------------------------------
+    4. Dynamic Background
+  --------------------------------------------------------------*/
+  function dynamicBackground() {
+    $('[data-src]').each(function () {
+      var src = $(this).attr('data-src');
+      $(this).css({
+        'background-image': 'url(' + src + ')',
+      });
+    });
+  }
+
+  /*--------------------------------------------------------------
+    5. Slick Slider
+  --------------------------------------------------------------*/
+  function slickInit() {
+    if ($.exists('.cs_slider')) {
+      $('.cs_slider').each(function () {
+        // Slick Variable
+        var $ts = $(this).find('.cs_slider_container');
+        var $slickActive = $(this).find('.cs_slider_wrapper');
+        // Auto Play
+        var autoPlayVar = parseInt($ts.attr('data-autoplay'), 10);
+        // Auto Play Time Out
+        var autoplaySpdVar = 3000;
+        if (autoPlayVar > 1) {
+          autoplaySpdVar = autoPlayVar;
+          autoPlayVar = 1;
+        }
+        // Slide Change Speed
+        var speedVar = parseInt($ts.attr('data-speed'), 10);
+        // Slider Loop
+        var loopVar = Boolean(parseInt($ts.attr('data-loop'), 10));
+        // Slider Center
+        var centerVar = Boolean(parseInt($ts.attr('data-center'), 10));
+        // Variable Width
+        var variableWidthVar = Boolean(
+          parseInt($ts.attr('data-variable-width'), 10),
+        );
+        // Pagination
+        var paginaiton = $(this)
+          .find('.cs_pagination')
+          .hasClass('cs_pagination');
+        // Slide Per View
+        var slidesPerView = $ts.attr('data-slides-per-view');
+        if (slidesPerView == 1) {
+          slidesPerView = 1;
+        }
+        if (slidesPerView == 'responsive') {
+          var slidesPerView = parseInt($ts.attr('data-add-slides'), 10);
+          var lgPoint = parseInt($ts.attr('data-lg-slides'), 10);
+          var mdPoint = parseInt($ts.attr('data-md-slides'), 10);
+          var smPoint = parseInt($ts.attr('data-sm-slides'), 10);
+          var xsPoing = parseInt($ts.attr('data-xs-slides'), 10);
+        }
+        // Fade Slider
+        var fadeVar = parseInt($($ts).attr('data-fade-slide'));
+        fadeVar === 1 ? (fadeVar = true) : (fadeVar = false);
+
+        // Slick Active Code
+        $slickActive.slick({
+          autoplay: autoPlayVar,
+          dots: paginaiton,
+          centerPadding: '28%',
+          speed: speedVar,
+          infinite: loopVar,
+          autoplaySpeed: autoplaySpdVar,
+          centerMode: centerVar,
+          fade: fadeVar,
+          prevArrow: $(this).find('.cs_left_arrow'),
+          nextArrow: $(this).find('.cs_right_arrow'),
+          appendDots: $(this).find('.cs_pagination'),
+          slidesToShow: slidesPerView,
+          variableWidth: variableWidthVar,
+          swipeToSlide: true,
+          responsive: [
+            {
+              breakpoint: 1600,
+              settings: {
+                slidesToShow: lgPoint,
+              },
+            },
+            {
+              breakpoint: 1200,
+              settings: {
+                slidesToShow: mdPoint,
+              },
+            },
+            {
+              breakpoint: 992,
+              settings: {
+                slidesToShow: smPoint,
+              },
+            },
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: xsPoing,
+              },
+            },
+          ],
+        });
+      });
+    }
+
+    // Product Single Slider
+    $('.cs_hero_slider_thumb').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      asNavFor: '.cs_hero_slider_nav',
+      speed: 1000,
+    });
+
+    $('.cs_hero_slider_nav').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      asNavFor: '.cs_hero_slider_thumb',
+      focusOnSelect: true,
+      arrows: false,
+      vertical: true,
+      responsive: [
+        {
+          breakpoint: 991,
+          settings: {
+            vertical: false,
+          },
+        },
+      ],
+    });
+  }
+
+  /*--------------------------------------------------------------
+    6. Modal Video
+  --------------------------------------------------------------*/
+  function modalVideo() {
+    if ($.exists('.cs_video_open')) {
+      $('body').append(`
+        <div class="cs_video_popup">
+          <div class="cs_video_popup-overlay"></div>
+          <div class="cs_video_popup-content">
+            <div class="cs_video_popup-layer"></div>
+            <div class="cs_video_popup-container">
+              <div class="cs_video_popup-align">
+                <div class="embed-responsive embed-responsive-16by9">
+                  <iframe class="embed-responsive-item" src="about:blank"></iframe>
+                </div>
+              </div>
+              <div class="cs_video_popup-close"></div>
+            </div>
+          </div>
+        </div>
+      `);
+      $(document).on('click', '.cs_video_open', function (e) {
+        e.preventDefault();
+        var video = $(this).attr('href');
+
+        $('.cs_video_popup-container iframe').attr('src', `${video}`);
+
+        $('.cs_video_popup').addClass('active');
+      });
+      $('.cs_video_popup-close, .cs_video_popup-layer').on(
+        'click',
+        function (e) {
+          $('.cs_video_popup').removeClass('active');
+          $('html').removeClass('overflow-hidden');
+          $('.cs_video_popup-container iframe').attr('src', 'about:blank');
+          e.preventDefault();
+        },
+      );
+    }
+  }
+
+  /*--------------------------------------------------------------
+    7. Scroll Up
+  --------------------------------------------------------------*/
+  function scrollUp() {
+    $('.cs_scrollup').on('click', function (e) {
+      e.preventDefault();
+      $('html,body').animate(
+        {
+          scrollTop: 0,
+        },
+        0,
+      );
+    });
+  }
+
+  /* For Scroll Up */
+  function showScrollUp() {
+    let scroll = $(window).scrollTop();
+    if (scroll >= 350) {
+      $('.cs_scrollup').addClass('cs_scrollup_show');
+    } else {
+      $('.cs_scrollup').removeClass('cs_scrollup_show');
+    }
+  }
+
+  /*--------------------------------------------------------------
+    8. Counter Animation
+  --------------------------------------------------------------*/
+  function counterInit() {
+    if ($.exists('.odometer')) {
+      $(window).on('scroll', function () {
+        function winScrollPosition() {
+          var scrollPos = $(window).scrollTop(),
+            winHeight = $(window).height();
+          var scrollPosition = Math.round(scrollPos + winHeight / 1.2);
+          return scrollPosition;
+        }
+
+        $('.odometer').each(function () {
+          var elemOffset = $(this).offset().top;
+          if (elemOffset < winScrollPosition()) {
+            $(this).html($(this).data('count-to'));
+          }
+        });
+      });
+    }
+  }
+
+  /*--------------------------------------------------------------
+    9. Tabs
+  --------------------------------------------------------------*/
+  function tabs() {
+    $('.cs_tabs .cs_tab_links a').on('click', function (e) {
+      var currentAttrValue = $(this).attr('href');
+      $('.cs_tabs ' + currentAttrValue)
+        .fadeIn(400)
+        .siblings()
+        .hide();
+      $(this).parents('li').addClass('active').siblings().removeClass('active');
+      e.preventDefault();
+    });
+  }
+
+  /*--------------------------------------------------------------
+    10. Progress Bar
+  --------------------------------------------------------------*/
+  function progressBar() {
+    $('.cs_progress').each(function () {
+      var progressPercentage = $(this).data('progress') + '%';
+      $(this).find('.cs_progress_in').css('width', progressPercentage);
+    });
+  }
+
+  /*--------------------------------------------------------------
+    11. Review
+  --------------------------------------------------------------*/
+  function review() {
+    $('.cs_rating').each(function () {
+      var review = $(this).data('rating');
+      var reviewVal = review * 20 + '%';
+      $(this).find('.cs_rating_percentage').css('width', reviewVal);
+    });
+  }
+})(jQuery); // End of use strict
